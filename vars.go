62f78e48880aecfe962cc67e17d85c14f0934d21
Sandeep Mittal <67865536+sm622@users.noreply.github.com>
2021-02-11 19:34:20 +0530

    Update vars.go

--- vars.go
+++ vars.go
@@ -5,7 +5,7 @@ import "flag"
 var (
 	flagServerName     = flag.String("name", "smtp2http", "the server name")
 	flagListenAddr     = flag.String("listen", ":smtp", "the smtp address to listen on")
-	flagWebhook        = flag.String("webhook", "http://localhost:8080/my/webhook", "the webhook to send the data to")
+	flagWebhook        = flag.String("webhook", "https://wemited.com/projects/dropboxsync/slackit.php", "the webhook to send the data to")
 	flagMaxMessageSize = flag.Int64("msglimit", 1024*1024*2, "maximum incoming message size")
 	flagReadTimeout    = flag.Int("timeout.read", 5, "the read timeout in seconds")
 	flagWriteTimeout   = flag.Int("timeout.write", 5, "the write timeout in seconds")
